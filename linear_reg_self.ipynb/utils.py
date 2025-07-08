6e3721acfd308ffb062fc243497cf7910c9bfa49
ToiLaKiet <tuilakietday05@gmail.com>
2025-07-08 23:28:19 +0700

    first commit

--- linear_reg_self.ipynb/utils.py
+++ linear_reg_self.ipynb/utils.py
@@ -1,8 +1,17 @@
 import numpy as np
-def compute_cost(data):
-    pass
-
+#--Data Preprocessing Functions--
 def z_score_normalize(data_col):
     min_data = min(data_col)
     max_data = max(data_col)
     return (data_col - min_data) / (max_data - min_data)
+#--Linear Regression Functions--
+class LinearRegression():
+    def __init__(self):
+        pass
+    def train(self,x,y):
+        model = self
+        
+        
+        pass
+    pass
+    
