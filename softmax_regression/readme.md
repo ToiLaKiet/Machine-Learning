fe0b39d8c5dae729c1511e40985758af46de7428
ToiLaKiet <tuilakietday05@gmail.com>
2025-08-13 15:13:01 +0700

    update DenseLayer, SoftmaxRegression Initilization Logic

--- softmax_regression/readme.md
+++ softmax_regression/readme.md
@@ -1,4 +1,12 @@
 # Introduction 
 In this project, I built my own softmax regression model from scratch using NumPy. The model is trained on the MNIST dataset, which consists of images of handwritten digits. The goal is to classify these images into one of the ten digit classes (0-9).
----
-The model uses the softmax function to compute probabilities for each class and the cross-entropy loss function to measure the performance of the model. The training process involves updating the model parameters using gradient descent.
+# Directory Structure
+`utils.py`: Contains utility functions for data preprocessing and the Softmax Regression class.<br>
+`main.ipynb`: Jupyter Notebook that demonstrates how to use the Softmax Regression class to fit a model and make predictions.
+# Requirements
+I built this model from scratch using Python. The code does not require any external libraries, only some libraries like NumPy and Pandas for data manipulation.
+# Usage
+To use the Softmax Regression implementation, follow these steps:
+1. Clone the repository or download the files.
+2. Open the `main.ipynb` Jupyter Notebook.
+3. Run the notebook cells to see how the Softmax Regression model is created, trained, and used for predictions.
--- softmax_regression/utils.py
+++ softmax_regression/utils.py
@@ -1,21 +1,50 @@
 import numpy as np
-def sigmoid(x,others):
+# Activation Functions
+def softmax(x):
     tuso = np.e**(x)
-    mauso = np.sum(np.e**(others))
+    mauso = np.sum(np.e**(x))
     return tuso / mauso
 def reLU(x):
     return max(0,x)
+# Fully-connected layers' Functions
+class DenseLayer():
+    def __init__(self, in_dim, out_dim, activation = "linear"):
+        self.w = np.zeros(in_dim)
+        self.b = np.zeros(out_dim)
+        self.activation = activation
+    def forward_prop(self,data):
+        if self.activation != "relu":
+            z = np.dot(data,self.w) + self.b
+        else:
+            z = reLU(np.dot(data,self.w) + self.b)
+        return z
+    def calculate_gradient(self):
+        pass
+# Softmax Regression Functions
 class SoftmaxRegression():
     def __init__(self):
-        self.relu_layer = []
-        self.linear_layer = [[[0]*64]*20] # Linear layer representing y = w * x + b
+        self.relu_layer = DenseLayer(in_dim=64, out_dim=25, activation="relu")
+        self.linear_layer = DenseLayer(in_dim=25, out_dim=10, activation="linear")
     def predict(self,data): 
-        data = [[data]*10]
-        for i in range(len(data)):
-            a_1 = reLU(data[i])
-        a_1 = np.dot(data,self.linear_layer)
-    def cross_entropy_loss(self,x):
-        pass
+        data = [ data ] * self.relu_layer.w
+        a_1 = self.relu_layer.forward_prop(data)
+        a_2 = softmax(self.linear_layer.forward_prop(a_1))
+        return a_2
+    def cross_entropy_loss(self,props,labels):
+        '''   
+        props : To each data row, there is a propability to the ground truth label, this props array is the collection of the model's predicted proability for that ground truth
+        labels : A matrix for one hot encoded version of the ground truth labels.
+        ---
+        return a list of CE Loss for all classes
+        '''
+        props = np.array(props).flatten()
+        labels = np.array(labels).flatten()
+        CE_loss = np.zeros(10)
+        for i in range(labels.shape[0]):
+            label = np.argmax(labels[i])
+            prop = props[i]
+            CE_loss[label] += -(label*(np.log(prop)) + (1-label)*(np.log((1-prop))))
+        return np.mean(CE_loss)
     def calculate_gradients(self):
         pass
     def batch_gd_training(self,x):
